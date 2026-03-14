7d35e23faa3745f1601d8a147d69aa31bf1ee170
Варвара Самощева <samoschevav2008@gmail.com>
2026-03-14 21:47:30 +0000

    meow

---
+++ ist01_Samoscheva_VA_flowers.py
@@ -0,0 +1,56 @@
+Flower:
+> variety
+> color
+> size
+() water
+() cut
+() buy
+
+
+
+Rose: Flower
+> thorns
+> serrated leaves
+() gift
+() prick
+
+
+
+Pion: Flower
+> size of the brick
+Flower:
+> variety
+> color
+> size
+() water
+() cut
+() buy
+
+
+Rose: Flower
+> thorns
+> serrated leaves
+() gift
+() prick
+
+
+Pion: Flower
+> size of the brick
+> petals color
+() bloom
+() crumble
+
+
+Ranunkulyus: Flower
+> beautiful
+> expensive
+> poisonous
+() bloom
+() please
+
+
+
+Cactus: Flower
+> needles
+() можно забить и не поливать, сам вырастет
+() prick
