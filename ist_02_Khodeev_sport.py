31080b85141e2aa83e02c9285a7a81bb9fbac790
hodeika <mishakhodeev@gmail.com>
2026-03-14 12:21:20 +0000

    add

---
+++ ist_02_Khodeev_sport.py
@@ -0,0 +1,24 @@
+sport
+>name
+>origin country
+>duration
+>number of participants
+()train
+()compete
+()win
+
+tennis:sport
+>number of sets
+>court type(clay, grass, hard)
+>racket size
+()serve
+()return
+()win set
+
+squash:tennis
+>wall material
+>ball type(slow, fast)
+>court size
+()hit the wall
+()drop shot
+()win rally
