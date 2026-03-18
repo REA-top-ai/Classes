ad34a1d825a173a20289f110219890525e8b5227
asdfopa <132782776+asdfopa@users.noreply.github.com>
2026-03-18 12:17:56 +0000

    classes pies

---
+++ ist_02_KizhapkinIS.py
@@ -0,0 +1,17 @@
+pies
+> purchased_in_a_store
+> must_to_be_prepared
+() prepare
+() eat
+
+medovik: pies
+> weight
+() honey_is_used_in_cooking
+
+сharlotte: pies
+> cost
+() apples_is_used_in_cooking
+
+kulebyak: pies
+> sell
+() different_ingredients_in_different_layers
