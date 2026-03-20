81126f2dd02ef135987ec34bab351b1415c2ff6f
Golubev <golubevalex2007@gmail.com>
2026-03-20 18:00:38 +0300

    add Golubev transport IST02

---
+++ ist02_Golubev_transport.py
@@ -0,0 +1,25 @@
+transport
+> speed
+> fuel_type
+> capacity
+() move
+() refuel
+() stop
+
+car: transport
+> brand
+> num_doors
+() honk
+() park
+
+truck: transport
+> load_capacity
+> num_axles
+() load_cargo
+() unload_cargo
+
+bicycle: transport
+> num_gears
+> frame_material
+() pedal
+() brake
