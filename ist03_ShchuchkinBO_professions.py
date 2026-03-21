b9e01998e6cca67a3a5e3c2e3d30b2087c43ba11
Krutoy_4elik <shchuchkinb07@gmail.com>
2026-03-21 18:30:54 +0300

    Add files via upload
    chtoto interesnoe

---
+++ ist03_ShchuchkinBO_professions.py
@@ -0,0 +1,17 @@
+profession
+>name
+>salary
+>worktime
+
+teacher:profession
+>subject
+>classes
+()teach
+()rate-homework
+()set-grades
+
+photographer:profession
+>camera-model
+>photography-specialisation
+()photoshoot
+()print-photos
