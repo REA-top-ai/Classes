a41e105c945420c726fe76e51e3f45add7d6f7e8
EEnota <malkovn824@gmail.com>
2026-03-21 14:19:47 +0000

    Malkov class

---
+++ ist_03_malkov_enote.py
@@ -0,0 +1,22 @@
+Enote
+>age
+>height
+>color
+()hide
+()catch
+
+enote_jr: Enote
+> toy
+()play
+()be_feeded
+
+enote_mdl : Enote
+> stone
+()hunt
+()feed
+()bite
+
+enote_sinor : Enote
+> stick
+()talk
+
